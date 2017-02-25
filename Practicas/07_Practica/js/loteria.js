b2d8f30f48a1fa9d820889147b548dd594e01d77
12030812 Cristian <matrix_kriztyan@live.com.mx>
2017-02-24 18:42:27 -0600

    Juego de Loteria

---
+++ Practicas/07_Practica/js/loteria.js
@@ -0,0 +1,20 @@
+$(document).ready(
+    function() {
+        console.log("Greetings form jQuery");
+        //alert("Greetings from jQuery");
+        
+        $("img").click(
+            function() {
+                //$(this).width("20px"); //Reduces width of the clicked img
+                //$(this).hide("slow");
+                $(this).attr('src','img/cards/'+'01'+'.jpg');
+            }
+        );
+        
+        $("#btn-restart").click(
+            function() {
+                $("img").show("fast");
+            }
+        );
+    }
+); //Document Selector, this time we are selecting to jQuery element

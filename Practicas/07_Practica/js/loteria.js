d0221463676dcd289648f5009ed3fe804f817efc
12030812 Cristian <matrix_kriztyan@live.com.mx>
2017-03-05 13:57:15 -0600

    Loteria Practice contribution since Friday

--- Practicas/07_Practica/js/loteria.js
+++ Practicas/07_Practica/js/loteria.js
@@ -6,15 +6,40 @@ $(document).ready(
         $("img").click(
             function() {
                 //$(this).width("20px"); //Reduces width of the clicked img
-                $(this).hide("slow");
+                //$(this).hide("slow");
                 //$(this).attr('src','img/cards/'+'01'+'.jpg');
+                $(this).css("filter", "grayscale(100%)");
+                var img_id = this.id;
             }
         );
-        
+
         $("#btn-restart").click(
             function() {
                 $("img").show("fast");
             }
         );
+        
+        /*
+        The next code block randomly changes the top image displayed from the 
+        available
+        */
+        var sort = [0,1,2,3,4,5,6,7,8,9];
+        
+        setInterval(
+            function() {
+                var rand = Math.floor((Math.random() * 10));
+                if(sort[rand]){
+                    $("#main").attr("src", "img/cards/" + rand + ".jpg");
+                    $("#"+rand).css("filter", "grayscale(100%)");
+                    sort[rand]= false;
+                }
+            }, 1000
+        );
+        
+        //The next function checks if all elements of aray are false
+        
+        
     }
+    
+    
 ); //Document Selector, this time we are selecting to jQuery element

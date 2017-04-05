81e051bfa7363a172ef9dab4cc5c440f4cc67848
12030812 Cristian <matrix_kriztyan@live.com.mx>
2017-04-05 17:43:25 -0500

    Practice 11 Using PHP

---
+++ Practicas/11_Practica/Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <!--This document was created to provide a blank file for future projects using Bootstratp-->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Plantilla</title><!--Insert Project Name-->
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/normalize.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet"><!--Define your own Style document-->
+    </head>
+    <body>
+        
+           
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        <!--Bootstrap Validator-->
+        <script src="js/validator.js"></script>
+    </body>
+</html>
+<?php>
+    #PHP data processing section
+?>

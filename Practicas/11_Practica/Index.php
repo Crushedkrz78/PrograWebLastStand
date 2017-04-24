862f5e3aa647e1af1d7ad150a17db45ecc2f91b1
12030812 Cristian <matrix_kriztyan@live.com.mx>
2017-04-23 23:58:39 -0500

    Practice 11 Update

--- Practicas/11_Practica/Index.php
+++ Practicas/11_Practica/Index.php
@@ -6,71 +6,56 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>PHP Formularios</title><!--Insert Project Name-->
+        <title>Solicitud de Empleo</title><!--Insert Project Name-->
         <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/normalize.css" rel="stylesheet">
-        <link href="css/custom.css" rel="stylesheet"><!--Define your own Style document-->
+        <link href="css/ustom.css" rel="stylesheet"><!--Define your own Style document-->
     </head>
     <body>
-        <div class="container-fluid"><!--Main Container-->
-            <div class="jumbotron"><!--Page Header-->
-                <h1>Práctica con PHP</h1>
-            </div><!--End of Page Header-->
-            <div class="row">
-                <form data-toggle="validator" role="form" action="Index.php" method="post">
-                    <div class="form-group">
-                        <div class="col-xs-4 col-xs-offset-4">
-                            <label>Ingrese un texto</label>
-                            <input class="form-control" type="text" id="test1" name="hola" placeholder="Ingrese texto..." required>    
-                        </div>
-                        <div class="col-xs-4 col-xs-offset-4">
-                            <button type="submit" class="btn btn-default">Finish</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div><!--End of Main container-->
-        
-        <div class="container-fluid">
-            <div class="header-container">
-                <div class="jumbotron">
-                    <h1>Practice form for Data Processing</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
+        <div class="container-fluid"><!--Main Container Start-->
+            <div class="row"><!--Job Application Start-->
+                <div class="col-xs-6">
                     <div class="row">
-                        <form action="methods/proc.php" method="post" data-toggle="validator" role="form">
-                            <div class="col-xs-4 col-xs-offset-2">
-                                <div class="form-group">
-                                    <label>Usuario</label>
-                                    <input type="text" class="form-control" name="user" placeholder="Usuario" required>
-                                </div>
-                                <div class="form-group">
-                                    <label>Contraseña</label>
-                                    <input type="password" class="form-control" name="password" placeholder="Contraseña" required>
-                                </div>
-                            </div>
-                            <div class="col-xs-4">
-                                <button type="submit" class="btn btn-default">Acceder</button>
-                            </div>
-                        </form>
+                        <div class="jumbotron">
+                            <h2>Solicitud de Empleo</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p>Nota: La información aquí proporcionada será tratada <br /> confidencialmente</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        
-        <?php
-            #print_r($_POST);
-            if(isset($_POST)){
-                print_r($_POST);
-            }
-            else
-            {
-                #NothingToDo
-            }
-        ?>
+                <div class="col-xs-4">
+                    <div class="row">
+                        
+                    </div>
+                    <div class="row">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                Puesto que solicita y turno de tu interés
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-2">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p>Inserte su fotografía</p>
+                        </div>
+                    </div>
+                </div>
+            </div><!--Job Application End-->
+            <div class="row"><!--Personal Data Start-->
+            </div><!--Personal Data End-->
+            <div class="row"><!--Family Data Start-->
+            </div><!--Family Data End-->
+        </div><!--Main Container End-->
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
---
+++ Practicas/11_Practica_A/methods/proc.php
@@ -0,0 +1,17 @@
+<?php
+/*
+This file was created for data processing
+This project inteds to practice data processing
+*/
+
+#EsteEsUn
+if(isset($_POST)){
+    print_r("Data Inserted");
+}
+else{
+    print_r("Data Not Inserted");
+}
+
+#PrintFormData
+print_r($_POST);
+?>
